e281af7800d625bbbb8bb2be0a1b7b2fbd3c1d70
Jonas <jonas.m.a.1997@gmail.com>
2023-06-29 16:54:27 -0300

    done

--- index.js
+++ index.js
@@ -55,7 +55,52 @@ io.on("connection", (socket) => {
 
     connection.connect(params);
   });
-  // Handle client disconnection
+
+  socket.on("multipleCommands", ({ ip, commands }) => {
+    console.log(ip, commands);
+    const connection = new Telnet();
+    const params = {
+      host: ip,
+      port: 23,
+      shellPrompt: "#",
+      //negotiationMandatory: false,
+      timeout: 5000,
+      execTimeout: 5000,
+      loginPrompt: "Username:",
+      passwordPrompt: "Password:",
+      username: "admin",
+      password: "OT#internet2018",
+      pageSeparator: "--More--",
+    };
+    connection.on("ready", function () {
+      console.log("Connected to Telnet server");
+      let i = 0;
+      const sendNextCommand = () => {
+        if (i < commands.length) {
+          // Send commands to the server
+          connection.exec(commands[i], async function (err, response) {
+            console.log(response);
+            io.emit("multipleResponses", response);
+            if (err) {
+              console.log(err);
+            }
+            i++;
+            sendNextCommand();
+          });
+        } else {
+          // Close the connection
+          connection.end();
+        }
+      };
+      sendNextCommand();
+    });
+
+    connection.on("close", function () {
+      console.log("Disconnected from Telnet server");
+    });
+
+    connection.connect(params);
+  });
 });
 
 const port = 3001;
