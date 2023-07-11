2bbb93feb8adf0ecea30f55db8bf0a442e6dea9c
Jonas <jonas.m.a.1997@gmail.com>
2023-07-11 14:47:25 -0300

    ...

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "http://177.200.131.54:3000",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
