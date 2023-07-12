f98272ca3819bf627ac8c57a51fbfaac1fe550d3
Jonas <jonas.m.a.1997@gmail.com>
2023-07-12 00:14:10 -0300

    ...

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: "*",
+});
 const { Telnet } = require("telnet-client");
 
 // Define the Telnet server information
