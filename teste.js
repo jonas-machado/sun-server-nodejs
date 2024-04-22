9df27ba287a9cc8ab99027d2cbf95585460c70b6
Jonas <jonas.m.a.1997@gmail.com>
2024-04-22 15:51:05 -0300

    ...

--- teste.js
+++ teste.js
@@ -4,22 +4,21 @@ const host = "100.64.128.75"; // Substitua pelo IP da sua antena WOM 5000
 const community = "public"; // Substitua pela community string da sua antena
 const port = 161; // Porta padrão SNMP
 
-const session = new snmp.Session({
-  host: host,
-  port: port,
-  community: community,
-});
+var session = snmp.createSession(host, community);
 
-const oid = ["1.3.6.1.2.1.1.1.0"]; // OID para status da antena
+const oids = ["1.3.6.1.4.1.26138.3.1.0"]; // OID para status da antena
 
-session.get(oid, (err, result) => {
-  if (err) {
-    console.error("Erro ao obter status da antena:", err);
-    return;
+session.get(oids, function (error, varbinds) {
+  if (error) {
+    console.error(error);
+  } else {
+    for (var i = 0; i < varbinds.length; i++) {
+      if (snmp.isVarbindError(varbinds[i])) {
+        console.error(snmp.varbindError(varbinds[i]));
+      } else {
+        console.log(varbinds[i].oid + " = " + varbinds[i].value);
+      }
+    }
   }
-
-  const status = result.variables[0].value;
-  console.log("Status da antena:", status);
+  session.close();
 });
-
-session.close();
