1bddfb3ddc3aab7150e4683dc81b7949e3965230
Jonas <jonas.m.a.1997@gmail.com>
2024-04-24 15:53:38 -0300

    ...

--- teste.js
+++ teste.js
@@ -1,12 +1,12 @@
 const snmp = require("net-snmp");
 
-const host = "100.64.128.75"; // Substitua pelo IP da sua antena WOM 5000
-const community = "public"; // Substitua pela community string da sua antena
+const host = "172.16.45.3"; // Substitua pelo IP da sua antena WOM 5000
+const community = "OT#2023"; // Substitua pela community string da sua antena
 const port = 161; // Porta padrão SNMP
 
 var session = snmp.createSession(host, community);
 
-const oids = ["1.3.6.1.4.1.26138.3.1.0"]; // OID para status da antena
+const oids = ["1.3.6.1.4.1.3902"];
 
 session.get(oids, function (error, varbinds) {
   if (error) {
